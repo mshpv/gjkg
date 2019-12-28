81028c75f7a5fcca4daaf916c0a2d569f6cf6b7b
mshpv <46712856+mshpv@users.noreply.github.com>
2019-12-28 14:39:38 +0200

    Вывод обработчика, подключение шаблонов писем

---
+++ lettertemplate/water.php
@@ -0,0 +1,17 @@
+<?php
+
+$output = "<html>
+<head>
+<title>Заявление</title>
+</head>
+<body>
+
+Вас зовут: $name<br />
+Ваш адрес ул. $street д. $building, кв. $appartments<br />
+Ваша проблема:  $problems
+
+</body>
+</html>";
+
+return $output;
+?>
--- php/submit.php
+++ php/submit.php
@@ -1,5 +1,18 @@
 <?php
-if(isset($_POST['firstname']) && isset($_POST['eduform']) && 
-isset($_POST['comment']) && isset($_POST['courses'])) 
+if(isset($_POST['name']) && isset($_POST['street']) && 
+isset($_POST['building']) && isset($_POST['appartments']) && isset($_POST['problems'])) 
 {
+    $name = htmlentities($_POST['name']);
+    $street = htmlentities($_POST['street']);
+    $building = htmlentities($_POST['building']);
+    $appartments = htmlentities($_POST['appartments']);
+    $problems = htmlentities($_POST['problems']);
+    $output = include 'lettertemplate/water.php';
+    echo $output;
+}
+else
+{   
+    echo "Введенные данные некорректны";
+}
+?>
     
